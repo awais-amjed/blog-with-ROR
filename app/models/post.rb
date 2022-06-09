617c11af1cc034b2cb0da22a21e27f1b5abbd301
Awais Amjed <awaisamjed171@gmail.com>
2022-06-09 18:14:39 +0500

    Add required functions in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def increment_posts_counter
+    current_user = User.find(user_id)
+    current_user.update(posts_counter: (current_user.posts_counter || 0) + 1)
+  end
+
+  def recent_comments(count: 5)
+    Comment.where(post_id: id).order(created_at: :desc)[0, count]
+  end
 end
