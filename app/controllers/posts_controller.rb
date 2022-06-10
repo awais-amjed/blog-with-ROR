0b8f460e2dbb9923cd758fb69b06e8a01392608e
Awais Amjed <awaisamjed171@gmail.com>
2022-06-10 23:09:21 +0500

    Add functionality to add new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @user = current_user
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: } }
+    end
+  end
+
+  def create
+    @user = current_user
+    post = Post.new(params.require(:post).permit(:text, :title))
+    puts post.user_id = @user.id
+    respond_to do |format|
+      format.html do
+        if post.save
+          post.increment_counter
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_posts_path(@user)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new, locals: { post: }
+        end
+      end
+    end
+  end
+
   def get_user_name(user_id)
     User.find(user_id).name
   end
