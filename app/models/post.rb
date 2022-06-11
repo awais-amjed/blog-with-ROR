05d06fa670ebca76bcac17fb9d7a2b30fc8664f0
Awais Amjed <awaisamjed171@gmail.com>
2022-06-11 12:26:58 +0500

    Add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def increment_counter
     current_user = User.find(user_id)
     current_user.update(posts_counter: (current_user.posts_counter || 0) + 1)
