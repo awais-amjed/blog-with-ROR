d1412b495a11c323aa8773a20f49f301ddbd9b16
sboursen <dev.sboursen@gmail.com>
2022-06-14 15:42:19 +0100

    Implement Comment authorization functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,8 +9,12 @@ class Ability
     can :read, Post
     can :read, User
     return unless user.present?
+
+    can :create, Comment
+    can :destroy, Comment, user_id: user.id
     can :manage, Post, user_id: user.id
     return unless user.is? :admin
+
     can :manage, :all
   end
 end
